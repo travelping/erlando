b4795661b73ed43e23a446e1cf61ff3b7a9d5c9b
eryx67 <eryx67@gmail.com>
2019-03-14 17:34:57 +0500

    add cont_m

---
+++ src/cont_m.erl
@@ -0,0 +1,26 @@
+-module(cont_m).
+
+-compile(export_all).
+
+-export_type([cont/1]).
+
+-export(['>>='/2, return/1, fail/1]).
+
+-behaviour(monad).
+
+-type cont(A) :: {cont, fun((fun ((A) -> any())) -> any())}.
+
+
+-spec '>>='(cont(A), fun( (A) -> cont(B) )) -> cont(B).
+'>>='({cont, X}, Fun) -> erlang:apply(X, [Fun]).
+
+-spec return(A) -> cont(A).
+return(X) ->
+    {cont, fun (F) -> erlang:apply(F, [X]) end}.
+
+
+-spec fail(A) -> cont(A).
+fail(X) -> error(X).
+
+run_cont({cont, C}) ->
+    erlang:apply(C, [fun (A) -> A end]).
